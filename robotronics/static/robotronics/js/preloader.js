f3d66266b6241021156bcc1cce9af18a5eb9394b
varunvaruns9 <varunjustrocks@gmail.com>
2018-02-17 10:59:57 +0530

    set preloader

---
+++ robotronics/static/robotronics/js/preloader.js
@@ -0,0 +1,10 @@
+var varvadhu
+
+function uptownfunc() {
+	varvadhu = setTimeout(pagedikhao, 4000);
+}
+
+function pagedikhao() {
+	document.getElementById("preloader").style.display = "none";
+	document.getElementById("theDiv").style.display = "block";
+}
